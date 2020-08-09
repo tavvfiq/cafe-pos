5fba5ce238defe32d0db74059fc16d93812e20ae
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-08-10 01:06:13 +0700

    update build

---
+++ build/precache-manifest.a4ffc4ea6bd5d63f3868bb2f9f722d84.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bb537c813d9bcfa906f561f900bb1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30056fd167700f02ea7a",
+    "url": "/static/css/main.86d7c4b2.chunk.css"
+  },
+  {
+    "revision": "872e3f6980e339a3e613",
+    "url": "/static/js/2.e3b71315.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e3b71315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30056fd167700f02ea7a",
+    "url": "/static/js/main.1bf77982.chunk.js"
+  },
+  {
+    "revision": "f9b527d21a1c28b8cc40",
+    "url": "/static/js/runtime-main.74f443bd.js"
+  },
+  {
+    "revision": "7833629751a5ff53ab3bbde1d8cef2e1",
+    "url": "/static/media/add.78336297.webp"
+  },
+  {
+    "revision": "dbbddd9708d4e21c30e85dcb38e43222",
+    "url": "/static/media/background.dbbddd97.webp"
+  },
+  {
+    "revision": "6033c397d72280e523c602294a3e0700",
+    "url": "/static/media/cart.6033c397.webp"
+  },
+  {
+    "revision": "8dd5314e34e71ecf133eac7562d28a62",
+    "url": "/static/media/check.8dd5314e.webp"
+  },
+  {
+    "revision": "528775ab39a5b175e70adecd6e892848",
+    "url": "/static/media/empty_cart.528775ab.webp"
+  },
+  {
+    "revision": "7f18329881662ce5b1ad51e1f00f0f6a",
+    "url": "/static/media/espresso.7f183298.webp"
+  },
+  {
+    "revision": "79bb5b4e8b5c329a0fecdfe186562178",
+    "url": "/static/media/foods.79bb5b4e.webp"
+  },
+  {
+    "revision": "0d7463bf0560e276980a23010363cc45",
+    "url": "/static/media/history.0d7463bf.webp"
+  },
+  {
+    "revision": "40a8a89713a0924a153a6de011c62c85",
+    "url": "/static/media/menu.40a8a897.webp"
+  },
+  {
+    "revision": "03653d06fb2cf681f8f2b08cc3233da9",
+    "url": "/static/media/search.03653d06.webp"
+  }
+]);
